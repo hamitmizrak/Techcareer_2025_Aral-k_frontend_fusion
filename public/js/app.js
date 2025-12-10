e0c876f67f91c5531fb959677c953a786f59f247
Hamit Mızrak <hamitmizrak@gmail.com>
2025-12-10 11:45:35 +0300

    initLoginFormValidation

--- public/js/app.js
+++ public/js/app.js
@@ -85,6 +85,70 @@ function initRegisterFormValidation() {
 } // end of initRegisterFormValidation
 
 // Login form validation
+function initLoginFormValidation() {
+  // Login Form üzerinden DOM elemanlarını seç
+  const loginForm = document.getElementById('loginForm');
+
+  // Eğer sayfa Login Form değilse, fonksiyondan çık
+  if (!loginForm) {
+    return;
+  }
+
+  // Form submit olayısını yakalasın
+  loginForm.addEventListener('submit', function (event) {
+    // Browser'ın varsayılan form submit davranışını engelle
+    event.preventDefault();
+
+    // formData: { name: '', email: '', password: '', confirmPassword: '' },
+    // Register form alanlarındaki input verileri seç
+    const emailInput = document.getElementById('email');
+    const passwordInput = document.getElementById('password');
+
+    // Form genel geçerlilik durumu
+    let isValid = true;
+
+    // email alanınını en az 8 karkter olup olmadığını kontrol etsin
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailInput.value || emailRegex.test || emailInput.value.trim().length < 8) {
+      isValid = false;
+      alert('Email en az 8 karakter olmalıdır.');
+      emailInput.classList.add('is-invalid');
+      emailInput.classList.remove('is-invalid');
+    } else {
+      emailInput.classList.remove('is-invalid');
+      emailInput.classList.add('is-valid');
+    }
+
+    // password alanınını en az 6 karakter olup olmadığını kontrol etsin
+    if (!passwordInput.value || passwordInput.value.trim().length < 6) {
+      isValid = false;
+      alert('Şifre en az 6 karakter olmalıdır.');
+      passwordInput.classList.add('is-invalid');
+      passwordInput.classList.remove('is-invalid');
+    } else {
+      passwordInput.classList.remove('is-invalid');
+      passwordInput.classList.add('is-valid');
+    }
+
+    // Eğer form gerçli değilse, submit sen dur hiç bir şey yapma  kullancıya hata gösteriyorum
+    // if(isValid === false) {
+    //   return;
+    // }
+    if (!isValid) {
+      return;
+    }
+
+    // Eğer tüm validasyonlar geçtiyse, formu submit et
+    loginForm.submit();
+  }); // end LoginForm submit event listener
+} // end of initLoginFormValidation
+
+
+// Sayfa yüklendiğinde form validasyonlarını başlat
+document.addEventListener('DOMContentLoaded', function () {
+  initRegisterFormValidation();
+  initLoginFormValidation();
+});
 
 
 
